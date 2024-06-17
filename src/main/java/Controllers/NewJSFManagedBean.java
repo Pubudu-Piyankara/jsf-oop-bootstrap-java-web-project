5f59448586f61238378f97bc5820b87b41f930ae
Piyankara N W M G P <pubudupiyankara.me@gmail.com>
2024-06-17 09:05:32 +0530

    first commit

---
+++ src/main/java/Controllers/NewJSFManagedBean.java
@@ -0,0 +1,18 @@
+
+package Controllers;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+
+@Named(value = "newJSFManagedBean")
+@Dependent
+public class NewJSFManagedBean {
+
+    /**
+     * Creates a new instance of NewJSFManagedBean
+     */
+    public NewJSFManagedBean() {
+    }
+    
+}
